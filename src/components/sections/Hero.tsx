09e7536de938a4fb7fe32209a2aa8b9c57473498
Esther Manrique <105511137+EstherManriqueGlez@users.noreply.github.com>
2026-08-01 13:26:51 -0600

    feat: update Hero component styles and logo handling

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,9 +1,16 @@
 import { motion } from 'framer-motion';
 import { Button } from '@/components/ui/Button/Button';
+
 import { GradientText } from '@/components/ui/GradientText/GradientText';
+import logo_pos from '@/assets/images/logo_pos.png';
+import logo_neg from '@/assets/images/logo_neg.png';
+
 import styles from './Hero.module.scss';
+import { useTheme } from '@/context/useTheme';
 
 export const Hero = () => {
+  const { theme } = useTheme();
+
   return (
     <section className={styles.hero}>
       <motion.div
@@ -12,13 +19,20 @@ export const Hero = () => {
         transition={{ duration: 0.8 }}
         className={styles.content}
       >
+        <div>
+          <img
+            src={theme === 'dark' ? logo_neg : logo_pos}
+            alt="Manrique logo"
+            className={styles.logoImg}
+          />
+        </div>
         <h1 className={styles.title}>
           Building <GradientText>intelligent interfaces</GradientText> with purpose.
         </h1>
         <p className={styles.subtitle}>Frontend Software Engineer</p>
         <div className={styles.actions}>
           <a href="#projects" className={styles.actionsLink}>
-            <Button>VIEW MY WORK</Button>
+            <Button>View my Work</Button>
           </a>
         </div>
       </motion.div>
